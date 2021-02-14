c8552d7124325946392975fb929b3b5fbefe64ab
harris-2374 <41405364+harris-2374@users.noreply.github.com>
2021-02-14 16:06:34 -0600

    init commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+# with open(os.path.join(here, 'README.rst')) as f:
+#       long_description = f.read()
+
+def get_version(string):
+      """ Parse the version number variable __version__ from a script. """
+      import re
+      version_re = r"^__version__ = ['\"]([^'\"]*)['\"]"
+      version_str = re.search(version_re, string, re.M).group(1)
+      return version_str
+
+
+setup(name='thex',
+    #   version=get_version(open('src/svim_asm/svim-asm').read()),
+      description='',
+    #   long_description=long_description,
+    #   url='https://github.com/eldariont/svim-asm',
+      author='Andrew Harris',
+      author_email='ajharris.2374@gmail.com',
+      license='GPLv3',
+      classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Environment :: Console',
+      'Intended Audience :: Science/Research',
+      'Topic :: Scientific/Engineering :: Bio-Informatics',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Programming Language :: Python :: 3.6'
+      ],
+      keywords='svim-asm SV assembly structural variation caller',
+      packages = find_packages("thex"),
+      package_dir = {"": "thex"},
+      data_files = [("", ["LICENSE"])],
+      zip_safe=False,
+      scripts=['thex/app.py'])
